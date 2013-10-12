f4629cdb7f4925507d66ad01fae68076d726a9db
James Robertson <james@r0bertson.co.uk>
2013-10-12 22:04:33 +0100

    bug fix: options is now set to @options

--- lib/activity-logger.rb
+++ lib/activity-logger.rb
@@ -7,12 +7,13 @@ require 'dynarex-daily'
 class ActivityLogger
   
   def initialize(path=nil, options={})
+    @options = options
     Dir.chdir(path) if path    
   end
 
   def create(desc='', time=Time.now)
     sleep 0.8
-    DynarexDaily.new(nil,options).create(time: time.to_s, desc: desc).save
+    DynarexDaily.new(nil, @options).create(time: time.to_s, desc: desc).save
   end
   
   # Returns true if the time from the last entry exceeds 45 minutes.
