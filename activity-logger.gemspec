d5dd56d5fad73705b8a26cfb0be125a90948a2a6
James Robertson <james@r0bertson.co.uk>
2015-03-18 20:32:09 +0000

    feature: can now generate an HTML file.

--- activity-logger.gemspec
+++ activity-logger.gemspec
@@ -1,14 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'activity-logger'
-  s.version = '0.1.15'
-  s.summary = 'activity-logger'
+  s.version = '0.2.0'
+  s.summary = 'Uses the Dynarex-daily gem to log notices for the day'
   s.authors = ['James Robertson']
   s.files = Dir['lib/**/*.rb']
-  s.add_dependency('dynarex-daily')
- 
+  s.add_runtime_dependency('dynarex-daily', '~> 0.1', '>=0.1.13')
+  s.add_runtime_dependency('simple-config', '~> 0.3', '>=0.3.0') 
   s.signing_key = '../privatekeys/activity-logger.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
   s.email = 'james@r0bertson.co.uk'
   s.homepage = 'https://github.com/jrobertson/activity-logger'
+  s.required_ruby_version = '>= 2.1.2'
 end
--- lib/activity-logger.rb
+++ lib/activity-logger.rb
@@ -3,14 +3,27 @@
 # file: activity-logger.rb
 
 require 'dynarex-daily'
+require 'simple-config'
 
 class ActivityLogger
 
   attr_writer :xml_instruction
   
-  def initialize(path=nil, options={})
+  def initialize(dir=nil, options={}, config: nil)
+    
     @options = options
-    Dir.chdir(path) if path    
+    @publish_html = false
+
+    
+    if config then
+      
+      h = SimpleConfig.new(config).to_h
+      dir, @urlbase, @edit_url, @css_url, @xsl_path = \
+                       %i(dir urlbase edit_url css_url, xsl_path).map{|x| h[x]}
+      @publish_html = true
+    end
+    
+    Dir.chdir(dir) if dir
   end
 
   def create(desc='', time=Time.now)
@@ -21,7 +34,12 @@ class ActivityLogger
 
     ddaily.create(time: time.to_s, desc: desc)
     ddaily.save
-
+    
+    if @publish_html then
+      File.write 'index.txt', ddaily.to_s
+      save_html() 
+    end
+    
   end
   
   # Returns true if the time from the last entry exceeds 45 minutes.
@@ -34,5 +52,46 @@ class ActivityLogger
       true
     end
   end
+  
+  private
 
+  def add(summary, name, s)
+    summary.add Rexle::Element.new(name).add_text s
+  end
+  
+  def render_html(doc)
+    
+    #lib = File.exists?('notices.xsl') ? '.' : File.dirname(__FILE__)
+    #xslt_buffer = File.read(File.join(lib,'notices.xsl'))
+    xslt_buffer = File.read @xsl_path
+
+    xslt  = Nokogiri::XSLT(xslt_buffer)
+    out = xslt.transform(Nokogiri::XML(doc.xml))
+    File.write 'index.html', out    
+  end
+  
+  def save_html()
+    
+    newfile = 'formatted.xml'
+    FileUtils.cp 'dynarexdaily.xml', newfile
+    doc = Rexle.new File.read(newfile)
+    
+    summary = doc.root.element('summary')
+
+    add summary, 'edit_url', "%s/index.txt" % [@edit_url]
+    add summary, 'date',      Date.today.strftime("%d-%b-%Y").upcase
+    add summary, 'css_url',   @css_url
+    add summary, 'published', Time.now.strftime("%d-%m-%Y %H:%M")    
+    
+    doc.root.xpath('records/entry') do |entry|
+      
+      e = entry.element('time')
+      e.text = Time.parse(e.text).strftime("%-l:%M%P")
+    end
+    
+    File.write newfile, doc.xml(pretty: true)    
+    render_html doc
+  end
+
+end
 end
